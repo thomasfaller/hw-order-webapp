d4e8409243ac8653c78b0c23bcd4b74b8c842065
Thomas Faller <thomasfaller@gmail.com>
2020-07-09 10:45:42 +0100

    added Material UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { Card, CardContent, Typography } from "@material-ui/core";
+
 export default function Home() {
-  return <div>Hello Youtube!!!</div>;
+  return <div>Hello Healthwave Order App :D</div>;
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="h1">Hello!</Typography>
+      </CardContent>
+    </Card>
+  );
 }
